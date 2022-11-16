3744fa68992225612fbd37f371963d8191390f0e
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2022-11-16 13:54:58 +0500

    done

--- Fron-end/src/index.js
+++ Fron-end/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Root/App';
+import {configureStore} from "@reduxjs/toolkit"
+import {Provider} from "react-redux"
+
+const store=configureStore({
+  reducer:{}
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store} >
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
