d70043bb355c982098cc37c5e513a62e8b72585c
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2022-11-16 15:40:05 +0500

    prduct

--- Fron-end/src/Components/features/ProductSlice.js
+++ Fron-end/src/Components/features/ProductSlice.js
@@ -1,21 +1,41 @@
-import { createSlice ,createAsyncThunk } from "@reduxjs/toolkit";
-import  axios from "axios"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
 
 const initialState={
-    itmes:[],
+    items:[],
     status:null,
 }
 
-export const ProductFetch=createAsyncThunk(
-    "products/productsFetch",
-    ()=>{
-    }
-    )
 
-const ProductsSlice=createSlice({
-    name:"Products",
+export const productsFetch=createAsyncThunk(
+    "products/productsFetch",
+   async ()=>{
+      const response=  await axios.get('http://localhost:5000/products')
+      return response?.data
+    }
+    );
+
+const productsSlice=createSlice({
+    name:"products",
     initialState,
     reducers:{},
+    extraReducers:{
+        [productsFetch.pending]:(state,action)=>{
+            state.status = "pending"
+        },
+        [productsFetch.fulfilled]:(state,action)=>{
+            state.status = "success";
+            state.items=action.payload;
+        },  
+        [productsFetch.rejected]:(state,action)=>{
+            state.status = "rejected";
+        
+        },
+    }
 })
+export default productsSlice.reducer
 
-export default ProductsSlice.reducer
+
+
+
--- Fron-end/src/index.js
+++ Fron-end/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Root/App';
+
 import {configureStore} from "@reduxjs/toolkit"
 import {Provider} from "react-redux"
 
-import ProductReducer from './Components/features/ProductSlice';
+import produtsReducer, { productsFetch } from './Components/features/ProductSlice';
 const store=configureStore({
   reducer:{
-    products: ProductReducer
+    products: produtsReducer
   }
 })
+store.dispatch(productsFetch())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
